09e88dab738f202e00454afaf760622b3d9bf820
mundusnine <mundusnine@gmail.com>
2023-07-21 18:07:04 -0400

    Add support to get which mouse button is down.Updated input example. Added possibility to detect screen size.

--- fenster.h
+++ fenster.h
@@ -18,6 +18,10 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+#define MOUSE_LEFT_BUTTON   0
+#define MOUSE_MIDDLE_BUTTON 1
+#define MOUSE_RIGHT_BUTTON  2
+
 struct fenster {
   const char *title;
   const int width;
@@ -43,6 +47,7 @@ struct fenster {
 #ifndef FENSTER_API
 #define FENSTER_API extern
 #endif
+FENSTER_API void fenster_screen_size(int *width, int *height);
 FENSTER_API int fenster_open(struct fenster *f);
 FENSTER_API int fenster_loop(struct fenster *f);
 FENSTER_API void fenster_close(struct fenster *f);
@@ -90,6 +95,10 @@ static BOOL fenster_should_close(id v, SEL s, id w) {
   (void)v, (void)s, (void)w;
   msg1(void, NSApp, "terminate:", id, NSApp);
   return YES;
+}
+
+FENSTER_API void fenster_screen_size(int *width, int *height) {
+  //NOT IMPLEMENTED
 }
 
 FENSTER_API int fenster_open(struct fenster *f) {
@@ -198,8 +207,22 @@ static LRESULT CALLBACK fenster_wndproc(HWND hwnd, UINT msg, WPARAM wParam,
     DestroyWindow(hwnd);
     break;
   case WM_LBUTTONDOWN:
+    f->mouse |= 1 << MOUSE_LEFT_BUTTON;
+    break;
   case WM_LBUTTONUP:
-    f->mouse = (msg == WM_LBUTTONDOWN);
+    f->mouse &= ~(1 << MOUSE_LEFT_BUTTON);
+    break;
+  case WM_MBUTTONDOWN:
+    f->mouse |= 1 << MOUSE_MIDDLE_BUTTON;
+    break;
+  case WM_MBUTTONUP:
+    f->mouse &= ~(1 << MOUSE_MIDDLE_BUTTON);
+    break;
+  case WM_RBUTTONDOWN:
+    f->mouse |= 1 << MOUSE_RIGHT_BUTTON;
+    break;
+  case WM_RBUTTONUP:
+    f->mouse &= ~(1 << MOUSE_RIGHT_BUTTON);
     break;
   case WM_MOUSEMOVE:
     f->y = HIWORD(lParam), f->x = LOWORD(lParam);
@@ -219,6 +242,14 @@ static LRESULT CALLBACK fenster_wndproc(HWND hwnd, UINT msg, WPARAM wParam,
     return DefWindowProc(hwnd, msg, wParam, lParam);
   }
   return 0;
+}
+
+FENSTER_API void fenster_screen_size(int *width, int *height) {
+  RECT desktop;
+  const HWND hDesktop = GetDesktopWindow();
+  GetWindowRect(hDesktop, &desktop);
+  *width = desktop.right;
+  *height = desktop.bottom;
 }
 
 FENSTER_API int fenster_open(struct fenster *f) {
@@ -259,6 +290,15 @@ FENSTER_API int fenster_loop(struct fenster *f) {
 // clang-format off
 static int FENSTER_KEYCODES[124] = {XK_BackSpace,8,XK_Delete,127,XK_Down,18,XK_End,5,XK_Escape,27,XK_Home,2,XK_Insert,26,XK_Left,20,XK_Page_Down,4,XK_Page_Up,3,XK_Return,10,XK_Right,19,XK_Tab,9,XK_Up,17,XK_apostrophe,39,XK_backslash,92,XK_bracketleft,91,XK_bracketright,93,XK_comma,44,XK_equal,61,XK_grave,96,XK_minus,45,XK_period,46,XK_semicolon,59,XK_slash,47,XK_space,32,XK_a,65,XK_b,66,XK_c,67,XK_d,68,XK_e,69,XK_f,70,XK_g,71,XK_h,72,XK_i,73,XK_j,74,XK_k,75,XK_l,76,XK_m,77,XK_n,78,XK_o,79,XK_p,80,XK_q,81,XK_r,82,XK_s,83,XK_t,84,XK_u,85,XK_v,86,XK_w,87,XK_x,88,XK_y,89,XK_z,90,XK_0,48,XK_1,49,XK_2,50,XK_3,51,XK_4,52,XK_5,53,XK_6,54,XK_7,55,XK_8,56,XK_9,57};
 // clang-format on
+FENSTER_API void fenster_screen_size(int *width, int *height){
+  Display *display = XOpenDisplay(NULL);
+
+  Screen *screen = XScreenOfDisplay(display,0);
+  *width = WidthOfScreen(screen);
+  *height = HeightOfScreen(screen);
+
+  XCloseDisplay(display);
+}
 FENSTER_API int fenster_open(struct fenster *f) {
   f->dpy = XOpenDisplay(NULL);
   int screen = DefaultScreen(f->dpy);
@@ -285,8 +325,10 @@ FENSTER_API int fenster_loop(struct fenster *f) {
     XNextEvent(f->dpy, &ev);
     switch (ev.type) {
     case ButtonPress:
+      f->mouse |= 1 << (ev.xbutton.button -1);
+      break;
     case ButtonRelease:
-      f->mouse = (ev.type == ButtonPress);
+      f->mouse &= ~(1 << (ev.xbutton.button -1));
       break;
     case MotionNotify:
       f->x = ev.xmotion.x, f->y = ev.xmotion.y;
